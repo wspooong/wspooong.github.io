b9645b3007b90b54a53148f10e5e8071000db36a
wspooong <wspooong@gmail.com>
2025-03-23 12:13:59 +0000

    Deploying to gh-pages from @ wspooong/wspooong.github.io@cf16fd61fc1d2faa7022e0770865d0bc4f8aae67 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,65 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-papers",
+          title: "papers",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/papers/";
+          },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "news-completed-master-s-degree-in-sociology-and-enter-cdna",
+          title: 'Completed Master’s degree in Sociology and enter CDNA.',
+          description: "",
+          section: "News",},{id: "projects-wip",
+          title: 'WIP',
+          description: "",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/wip_project/";
+            },},{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
